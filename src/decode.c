42e8492508f9f43fd3c11f01ef14e0fdeadf4106
HenryChaing <146239606+HenryChaing@users.noreply.github.com>
2024-02-01 15:52:32 +0800

    Update decode.c

--- src/decode.c
+++ src/decode.c
@@ -465,7 +465,7 @@ static inline bool op_op_imm(rv_insn_t *ir, const uint32_t insn)
     /* dispatch from funct3 field */
     switch (decode_funct3(insn)) {
     case 0: /* ADDI: Add Immediate */
-        /*
+        
         if(rd==15 && rs1==15){ir->opcode = rv_insn_addi015015;}
         else if(rd==14 && rs1==14){ir->opcode = rv_insn_addi014014;}
         else if(rd==2 && rs1==2){ir->opcode = rv_insn_addi0202;}
@@ -475,7 +475,8 @@ static inline bool op_op_imm(rv_insn_t *ir, const uint32_t insn)
         else if(rd==rs1 && imm==1){ir->opcode = rv_insn_inc;}
         else if(rd==rs1 && imm==-1){ir->opcode = rv_insn_dec;}
         else{ir->opcode = rv_insn_addi;}
-        */
+        
+        /*
         if(rd==rs1){
             if(rd==15){ir->opcode = rv_insn_addi015015;}
             else if(rd==14){ir->opcode = rv_insn_addi014014;}
@@ -488,7 +489,7 @@ static inline bool op_op_imm(rv_insn_t *ir, const uint32_t insn)
         else if(imm==0){ir->opcode = rv_insn_mv;}
         else if(rs1==0){ir->opcode = rv_insn_li;}
         else{ir->opcode = rv_insn_addi;}
-
+        */
         break;
     case 1: /* SLLI: Shift Left Logical */
         ir->opcode = rv_insn_slli;
