f64afee552215357ce237183623f40c870c72c5d
HenryChaing <146239606+HenryChaing@users.noreply.github.com>
2024-02-02 19:05:25 +0800

    Update decode.c

--- src/decode.c
+++ src/decode.c
@@ -464,8 +464,7 @@ static inline bool op_op_imm(rv_insn_t *ir, const uint32_t insn)
 
     /* dispatch from funct3 field */
     switch (decode_funct3(insn)) {
-    case 0: /* ADDI: Add Immediate */
-        
+    case 0: /* ADDI: Add Immediate */        
         if(rd==15 && rs1==15){ir->opcode = rv_insn_addi015015;}
         else if(rd==14 && rs1==14){ir->opcode = rv_insn_addi014014;}
         else if(rd==2 && rs1==2){ir->opcode = rv_insn_addi0202;}
@@ -473,8 +472,7 @@ static inline bool op_op_imm(rv_insn_t *ir, const uint32_t insn)
         else if(rs1==0){ir->opcode = rv_insn_li;}
         else if(rd==rs1 && imm==1){ir->opcode = rv_insn_inc;}
         else if(rd==rs1 && imm==-1){ir->opcode = rv_insn_dec;}
-        else{ir->opcode = rv_insn_addi;}
-        
+        else{ir->opcode = rv_insn_addi;}        
         break;
     case 1: /* SLLI: Shift Left Logical */
         ir->opcode = rv_insn_slli;
